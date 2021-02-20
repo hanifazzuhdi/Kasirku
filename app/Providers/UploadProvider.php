499d5678f59601253a75d0ec5ce2f498018e82ad
hanifazzuhdi <hnfhanif52@gmail.com>
2021-02-20 10:04:42 +0700

    try fix upload barang

--- app/Providers/UploadProvider.php
+++ app/Providers/UploadProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Http\Request;
+
+use GuzzleHttp\Client as GuzzleHttpClient;
 use Illuminate\Support\ServiceProvider;
 use Milon\Barcode\Facades\DNS1DFacade;
 use Milon\Barcode\Facades\DNS2DFacade;
@@ -27,7 +29,19 @@ class UploadProvider extends ServiceProvider
         if ($type == 'register') {
             $image = DNS2DFacade::getBarcodePNG($request, 'QRCODE');
         } else {
-            $image = DNS1DFacade::getBarcodePNG($request, 'C39', 1, 34);
+            $image = DNS1DFacade::getBarcodePNG($request, 'C39', 1, 34, array(1, 1, 1), true);
+
+            $client = new GuzzleHttpClient();
+            $response = $client->request('POST', 'https://api.imgbb.com/1/upload', [
+                'form_params' => [
+                    'key' => 'f98a15c0e84720165f5cd99516022338',
+                    'image' => $image,
+                    'name' => $request
+                ]
+            ]);
+            $res = json_decode($response->getBody()->getContents(), true);
+
+            return $res['data']['url'];
         }
 
         $response = cloudinary()->upload("data:image/png;base64,$image")->getSecurePath();
