1fc0666a41befc49fba7c10f063f950650fbdd29
hanifazzuhdi <hnfhanif52@gmail.com>
2021-02-20 10:44:17 +0700

    try fix upload barang

--- app/Providers/UploadProvider.php
+++ app/Providers/UploadProvider.php
@@ -28,10 +28,12 @@ class UploadProvider extends ServiceProvider
     {
         if ($type == 'register') {
             $image = DNS2DFacade::getBarcodePNG($request, 'QRCODE');
+
+            // dd($image);
         } else {
             $image = DNS1DFacade::getBarcodePNG($request, 'C39', 1, 34, array(1, 1, 1), true);
 
-            dd($image);
+            // dd($image);
 
             $client = new GuzzleHttpClient();
             $response = $client->request('POST', 'https://api.imgbb.com/1/upload', [
