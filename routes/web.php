46f6029776897ab226098dc34d375b6f21362b77
hanifazzuhdi <hnfhanif52@gmail.com>
2021-02-20 10:20:54 +0700

    try fix upload barang

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Milon\Barcode\Facades\DNS1DFacade;
 
 /*
 |--------------------------------------------------------------------------
