ee7ce9514677b065ba1300d57a3da5c4e44f95e0
hanifazzuhdi <hnfhanif52@gmail.com>
2021-02-20 10:23:03 +0700

    try fix upload barang

--- app/Providers/UploadProvider.php
+++ app/Providers/UploadProvider.php
@@ -31,12 +31,11 @@ class UploadProvider extends ServiceProvider
         } else {
             $image = DNS1DFacade::getBarcodePNG($request, 'C39', 1, 34, array(1, 1, 1), true);
 
-            // dd($image);
             $client = new GuzzleHttpClient();
             $response = $client->request('POST', 'https://api.imgbb.com/1/upload', [
                 'form_params' => [
                     'key' => 'f98a15c0e84720165f5cd99516022338',
-                    'image' => 'iVBORw0KGgoAAAANSUhEUgAAAMAAAAAiAQMAAAAznkTNAAAABlBMVEX///8BAQE6HLieAAAAAXRSTlMAQObYZgAAAAlwSFlzAAAOxAAADsQBlSsOGwAAAHtJREFUKJFj6N71TuvFuq53O3a9WPdi3Y5XL4D8xbu6dzGMSoxKwCXIBsyHGeQkDD4+Pt6AJsGWzmBsUTjbLOcAupZsBuOIjc0GGBLMh+U3Sxh+fHwWQ0ey/HwJw9kGeZgSDMwgCQyj2JIZjIESmJYznz8gV/cHi3OJBwCUypYvBuDH0QAAAABJRU5ErkJggg==',
+                    'image' => "$image",
                     'name' => $request
                 ]
             ]);
